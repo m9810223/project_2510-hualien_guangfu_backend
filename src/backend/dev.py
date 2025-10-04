764ea1b5b616331abf9f09a09e3bcf529b0a9ce4
Michael <m9810223@gmail.com>
2025-10-05 05:54:00 +0800

    WIP

--- src/backend/dev.py
+++ src/backend/dev.py
@@ -1,29 +1,26 @@
 import warnings
 
+from app.schemas.item_schema import CreateItemSchema
+from app.schemas.task_claim_schema import CreateTaskClaimSchema
+from app.schemas.task_schema import CreateTaskSchema
 from hypothesis import find
 from hypothesis import strategies as st
 from hypothesis.errors import NonInteractiveExampleWarning
-from pydantic import BaseModel
-from pydantic import Field
-from pydantic import NonNegativeInt
-from pydantic import PositiveFloat
 
 
 warnings.filterwarnings('ignore', category=NonInteractiveExampleWarning)
 
 
-class Model(BaseModel):
-    amount: NonNegativeInt = Field(le=400)
-    price: PositiveFloat = Field(le=40)
+for m in [
+    CreateItemSchema,
+    CreateTaskSchema,
+    CreateTaskClaimSchema,
+]:
+    example_1 = find(st.builds(m), lambda x: True)
+    print(f'{example_1=}')
 
+    example_2 = st.from_type(m).example()
+    print(f'{example_2=}')
 
-example_1 = find(st.builds(Model), lambda x: True)
-print(f'{example_1=}')
-
-
-example_2 = st.from_type(Model).example()
-print(f'{example_2=}')
-
-
-example_3 = st.builds(Model).example()
-print(f'{example_3=}')
+    example_3 = st.builds(m).example()
+    print(f'{example_3=}')
